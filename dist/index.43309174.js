00753a23bb01debbd0eb4a0003e0cbbb5d1be6a8
Whaleless <97296345+Whaleless@users.noreply.github.com>
2022-10-29 23:36:08 +0200

    parcelAdded

---
+++ dist/index.43309174.js
@@ -0,0 +1,14 @@
+const navButton = document.querySelector(".menu-button");
+const navMenu = document.querySelector(".navigation");
+navButton.addEventListener("click", ()=>{
+    const visibility = navMenu.getAttribute("data-visible");
+    if (visibility == "false") {
+        navMenu.setAttribute("data-visible", "true");
+        navButton.setAttribute("area-expanded", "true");
+    } else {
+        navMenu.setAttribute("data-visible", "false");
+        navButton.setAttribute("area-expanded", "false");
+    }
+});
+
+//# sourceMappingURL=index.43309174.js.map
---
+++ dist/index.45a629bb.js
@@ -0,0 +1,2 @@
+const navButton=document.querySelector(".menu-button"),navMenu=document.querySelector(".navigation");navButton.addEventListener("click",(()=>{"false"==navMenu.getAttribute("data-visible")?(navMenu.setAttribute("data-visible","true"),navButton.setAttribute("area-expanded","true")):(navMenu.setAttribute("data-visible","false"),navButton.setAttribute("area-expanded","false"))}));
+//# sourceMappingURL=index.45a629bb.js.map
